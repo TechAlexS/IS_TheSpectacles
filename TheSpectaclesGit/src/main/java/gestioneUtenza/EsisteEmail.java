3a8faedd7bcce4c80935ac535d187efedb3b9c90
Mario3107 <m.ranieri21@studenti.unisa.it>
2023-02-10 18:09:24 +0100

    @param EsisteEmail

--- TheSpectaclesGit/src/main/java/gestioneUtenza/EsisteEmail.java
+++ TheSpectaclesGit/src/main/java/gestioneUtenza/EsisteEmail.java
@@ -19,53 +19,54 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-
-
+/**
+ * Questa classe è un control che si occupa di passare la mail ad UtenteDao per vedere se esiste già in fase di registrazione 
+ */
 @WebServlet("/cercaMail")
 public class EsisteEmail extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static UtenteDao utenteModel = new UtenteDao();
+	private static final long serialVersionUID=1L;
+	private static UtenteDao utenteModel=new UtenteDao();
 	
-	
-	
-
+	/**
+	 * @return
+	 * @throws ServletException
+	 */
 	public void init() throws ServletException {
 		super.init();
-		utenteModel.setDB((DataSource) getServletContext().getAttribute("DataSource"));
-		
+		utenteModel.setDB((DataSource) getServletContext().getAttribute("DataSource"));	
 	}
 	
     public EsisteEmail() {
-        super();
-        
+        super();   
     }
+    
+    /**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect(response.encodeRedirectURL("error.jsp"));
 	}
+    
+    /**
+	 * @precondition request.getParameter(“email”)!=null 
+	 * @postcondition request.getSession().getAttribute(“email”)!=null
+	 * @throws ServletException, IOException
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email=request.getParameter("email");
+		System.out.println("sono in cerca email\n");
+		int ruolo=0;
+		PrintWriter out=response.getWriter();
 		
-		
-		String email= request.getParameter("email");
-		
-		System.out.println("SOno in cerca email\n");
-		
-		int ruolo= 0;
-		PrintWriter out= response.getWriter();
-		try {
-			
-			if(utenteModel.esisteEmail(email));
-			{
-				HttpSession session= request.getSession(false);
+		try {	
+			if(utenteModel.esisteEmail(email)){
+				HttpSession session=request.getSession(false);
 				session.setAttribute("email", email);
 				out.print("ok");
 			}
-			
-		} catch (SQLException e) {
+		} catch(SQLException e) {
 			System.out.println ("Errore nella signin: " + e.getMessage());
-		}
-		
-		
+		}	
 	}
-
 }
