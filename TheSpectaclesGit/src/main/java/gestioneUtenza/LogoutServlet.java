2de04f59d3cbdd5906ddd1460cb550956be7db8c
Mario3107 <m.ranieri21@studenti.unisa.it>
2023-02-10 17:47:57 +0100

    @param logoutServlet

--- TheSpectaclesGit/src/main/java/gestioneUtenza/LogoutServlet.java
+++ TheSpectaclesGit/src/main/java/gestioneUtenza/LogoutServlet.java
@@ -11,14 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+/**
+ * Questa classe è un control che si occupa di effettua il logout dell’utente dal sito 
+ */
 @WebServlet("/log-out")
 public class LogoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID=1L;
        
-   
+	/**
+	 * @precondition Request.getSession().getAttribute(“auth”)!=null
+	 * @postcondition request.getSession().getAttribute(“auth”)==null
+	 * @throws ServletException, IOException
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		try (PrintWriter out = response.getWriter()) {
+		try (PrintWriter out=response.getWriter()) {
 			if(request.getSession().getAttribute("auth")!=null) {
 				request.getSession().removeAttribute("auth");
 				request.getSession().invalidate();
@@ -26,12 +33,13 @@ public class LogoutServlet extends HttpServlet {
 			}else {
 				response.sendRedirect("index.jsp");
 			}
-
 		} 
 	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
