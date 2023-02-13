2247a1dac6a94dc7ecc5b9940441650b0e4ef6f4
Roberto Piscopo <r.piscopo8@studenti.unisa.it>
2023-02-13 21:57:57 +0100

    TestLogin finito

--- TheSpectaclesGit/src/test/java/gestioneUtenza/TestLoginServlet.java
+++ TheSpectaclesGit/src/test/java/gestioneUtenza/TestLoginServlet.java
@@ -2,7 +2,7 @@ package gestioneUtenza;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -30,7 +30,7 @@ public class TestLoginServlet {
  
   private static final String rightEmail = "a@gmail.com";
   private static final String badEmail = "test@libero.it";
-  private static final String rightPass = "Password123";
+  private static final String rightPass = "Password1234";
   private static final String badPass = "badPassword";
   
   
@@ -39,14 +39,15 @@ public class TestLoginServlet {
   private static final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
   private static final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
   private static final HttpSession session = Mockito.mock(HttpSession.class);
-  private static final PrintWriter out = Mockito.mock(PrintWriter.class);
+  
   
   
  
   @BeforeEach
   public void setUp() throws Exception {
-    //Mockito.doReturn(session).when(request).getSession();
+     Mockito.doReturn(session).when(request).getSession();
     //request.setSession(session);
+	 
   }
     
     
@@ -55,12 +56,12 @@ public class TestLoginServlet {
    
   }
  
- //TC_2.1_1 - Email non trovata nel database
+ //TC_1.2_1 - Email inesistente
   @Test
-  public void testCase_1() throws ServletException, IOException{
+  public void testCase_1_2_1() throws ServletException, IOException{
    
    
-   
+   PrintWriter out = Mockito.mock(PrintWriter.class);
    ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
 
    Mockito.when(request.getParameter("email")).thenReturn(badEmail);
@@ -73,5 +74,46 @@ public class TestLoginServlet {
    assertEquals("Nulla", argument.getValue());
    
      
+  }
+  
+//TC_1.2_2 - Password errata
+  @Test
+  public void testCase_1_2_2() throws ServletException, IOException{
+   
+   
+   PrintWriter out = Mockito.mock(PrintWriter.class);
+   ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+
+   Mockito.when(request.getParameter("email")).thenReturn(rightEmail);
+   Mockito.when(request.getParameter("password")).thenReturn(badPass);
+   Mockito.when(response.getWriter()).thenReturn(out);
+
+   servlet.doPost(request, response);
+
+   Mockito.verify(out).print(argument.capture());
+   assertEquals("Nulla", argument.getValue());
+   
+     
+  }
+  
+//TC_1.2_3 - Email e password Corretti
+  @Test
+  public void testCase_1_2_3() throws ServletException, IOException{
+   
+   
+   PrintWriter out = Mockito.mock(PrintWriter.class);
+   ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+
+   Mockito.when(request.getParameter("email")).thenReturn(rightEmail);
+   Mockito.when(request.getParameter("password")).thenReturn(rightPass);
+   Mockito.when(response.getWriter()).thenReturn(out);
+
+   servlet.doPost(request, response);
+
+   Mockito.verify(out).print(argument.capture());
+   //UtenteBean usr=(UtenteBean)session.getAttribute("auth");
+   //assertTrue( usr.getEmail().equals(rightEmail) );
+   assertEquals("Utente", argument.getValue());
+   
   }
 }
